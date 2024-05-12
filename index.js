cabda0c632ad84998bf51113a3df8390db800a51
Jimmy Patel <jimmypatel678@yahoo.com>
2024-05-12 18:05:44 -0400

    Changes made in index.js

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
-import serverless from "serverless-http";
 
 import connectDB from './Config/db.js';
 import authRoutes from './Routes/auth.route.js'
@@ -18,7 +17,6 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 
-
 app.use(
   cors({
     origin: ["http://localhost:5173", "https://jimmypatel.tech"],
@@ -26,12 +24,11 @@ app.use(
   })
 );
 
-
-
 app.use('/api/contact', contactRoutes);
 app.use('/api/post', postRoutes);
 app.use('/api/project', projectRoutes);
 app.use('/api/auth', authRoutes)
+
 // Error handling middleware
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
@@ -47,19 +44,5 @@ app.use('/', (req, res) => {
   res.send('Server is running');
 });
 
-
-const PORT = process.env.PORT || 3000;
-
-const startServer = async () => {
-  try {
-    await connectDB();
-    await cloudinaryConnect();
-    app.listen(PORT, () => console.log(`Server is listening on port ${PORT}`));
-  } catch (error) {
-    console.error('Error starting server:', error);
-    process.exit(1); // Exit with non-zero status code to indicate failure
-  }
-};
-
-startServer();
-export const handler = serverless(app);
+// Export the Express app
+export default app;
