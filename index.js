23e265839936c06dd818a7be5d769fe7a85928b1
Jimmy Patel <jimmypatel678@yahoo.com>
2024-12-07 13:32:20 -0500

    Transfer to Auth0

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ app.use(cookieParser());
 
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://jimmypatel.tech"],
+    origin: ["http://localhost:5173", "https://jimmypatel.tech", "*"],
     credentials: true,
   })
 );
